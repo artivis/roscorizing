8a70fbf61d4846429d3139bf2126b920a9ae4d9d
Enrique Fernandez <efernandez@clearpathrobotics.com>
2016-01-11 22:32:16 -0500

    Latch rostopic pub

--- scripts/test_roscorizing.sh
+++ scripts/test_roscorizing.sh
@@ -9,7 +9,7 @@ SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $SCRIPTS_DIR/roscorizing.sh
 
 # Run roscorizing in a subshell
-roscorizing rostopic pub /test_msg std_msgs/Bool "data: True" &
+roscorizing rostopic pub -l /test_msg std_msgs/Bool "data: True" &
 
 # Give some time to roscore to starts
 sleep 1
